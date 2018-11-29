36b0bf0efc2cf3a2a7ba70d3840547046c381bad
Amine M. Boulouma <amine@boulouma.com>
2018-11-29 16:21:15 +0100

    Added javadoc for Intersection Normale

--- src/main/java/modele/metier/IntersectionNormal.java
+++ src/main/java/modele/metier/IntersectionNormal.java
@@ -1,11 +1,29 @@
 package modele.metier;
 
+/** 
+ * La classe demande d'une intersection normale et non abstraite.
+ * @author H4404
+ * @version 1.0
+ * @since 1.0
+*/
+
+
 public class IntersectionNormal extends Intersection{
 	
+	/**
+	 * Constructeur d'une intersection normal avec identifiant.
+	 * @param id identifiant de l'intersection.
+	 */
 	public IntersectionNormal(long id) {
 		super(id);
 	}
 
+	/**
+	 * Constructeur paramétré d'une intersection normal.
+	 * @param id identifiant de l'intersection.
+	 * @param latitude latitude de l'intersection.
+	 * @param longitude longitude de l'intersection.
+	 */
 	public IntersectionNormal(long id, double latitude, double longitude) {
 		super(id,latitude,longitude);
 	}
