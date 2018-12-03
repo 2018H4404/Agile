01be04f7d18e4276608a71f70982d3708cf3ecdb
SHU <a806621313@gmail.com>
2018-12-03 10:51:13 +0100

    add test unitaire

--- src/test/java/testunitaire/TestTourneeManager.java
+++ src/test/java/testunitaire/TestTourneeManager.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import services.LecteurDeXML;
+import modele.services.LecteurDeXML;
 import controleur.Controleur;
 import modele.*;
 import modele.metier.*;
@@ -23,15 +23,21 @@ public class TestTourneeManager {
 	
 	@Test
 	public void testCalculerLesTournees1() throws Exception {
-		File fichierTestPlan = new File("fichiersXML2018/fichiersTest/testPlan.xml");	
+		File fichierTestPlan = new File("fichiersXML2018/fichiersTest/testTournee_plan2.xml");	
 		controleur.chargerFichierPlan(fichierTestPlan);
 		Plan unPlan = controleur.getMonPlan();
-		File fichierTestDemandeLivraison = new File("fichiersXML2018/fichiersTest/testPlan.xml");	
+		File fichierTestDemandeLivraison = new File("fichiersXML2018/fichiersTest/testTournee_dl2.xml");	
 		controleur.chargerFichierDemandeLivraison(fichierTestDemandeLivraison);
 		DemandeLivraison demande = controleur.getMaDemande();
 		tm.calculerLesTournees(demande, unPlan, 1);
+		int nbTournee = tm.getListeTournees().size();
+		assertEquals(nbTournee,1);
+		assertEquals(tm.getListeTournees().get(0).getListeChemins().size(),2);
 		
-		
+		System.out.println(tm.getListeTournees().get(0).getListeChemins().get(0).getListeIntersections().toString());
+		//System.out.println(tm.getListeTournees().get(0).getListeChemins().get(j).getListeTroncons().toString());
+		assertEquals(tm.getListeTournees().get(0).getListeChemins().get(0).getListeIntersections().size(),3);
+		assertEquals(tm.getListeTournees().get(0).getListeChemins().get(1).getListeIntersections().size(),3);
 	}
 	
 }
