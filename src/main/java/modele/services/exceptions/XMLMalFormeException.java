c1e577bbd34ad0cf7019c70997145ae8d8fb73c5
MainHanzo <yang.hua@insa-lyon.fr>
2018-12-13 18:43:19 +0100

    javadoc

--- src/main/java/modele/services/exceptions/XMLMalFormeException.java
+++ src/main/java/modele/services/exceptions/XMLMalFormeException.java
@@ -1,5 +1,10 @@
 package modele.services.exceptions;
-
+/** 
+ * La classe des exceptions lors on lire une fichier XML errone.
+ * @author H4404
+ * @version 1.0
+ * @since 1.0
+*/
 @SuppressWarnings("serial")
 public class XMLMalFormeException extends Exception {
 	static String EXCEPTION_MESSAGE = "Le fichier XML contient une erreur.(mal forme)";
